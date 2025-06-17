ce9157347b50c1c403cc4916edf3b28a5ef7d347
bhindor <bhindor@gmail.com>
2025-06-17 19:35:05 +0900

    ✅ :: viewBinding setting
    뷰 바인딩 세팅

--- app/src/main/java/kr/hs/anu/nobet/presentatoin/screen/main/MainActivity.kt
+++ app/src/main/java/kr/hs/anu/nobet/presentatoin/screen/main/MainActivity.kt
@@ -6,12 +6,16 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import kr.hs.anu.nobet.R
+import kr.hs.anu.nobet.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
