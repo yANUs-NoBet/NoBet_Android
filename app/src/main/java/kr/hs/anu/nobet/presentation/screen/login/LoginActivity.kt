0940c8fedc3bd21fdfa5bcbc7cf742a8a79feef9
bhindor <bhindor@dgsw.hs.kr>
2025-07-09 11:13:23 +0900

    ⚡️ :: connect login to main

---
+++ app/src/main/java/kr/hs/anu/nobet/presentation/screen/login/LoginActivity.kt
@@ -0,0 +1,26 @@
+package kr.hs.anu.nobet.presentation.screen.login
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kr.hs.anu.nobet.R
+import kr.hs.anu.nobet.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
