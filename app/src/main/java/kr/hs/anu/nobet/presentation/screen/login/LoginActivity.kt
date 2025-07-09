6ade225a7c2c48b96485a2bee8fc0fbf3b4e9d0f
bhindor <bhindor@dgsw.hs.kr>
2025-07-09 13:40:30 +0900

    ⚡️ :: join online counsel
    온라인 상담 확장함수 구현

--- app/src/main/java/kr/hs/anu/nobet/presentation/screen/login/LoginActivity.kt
+++ app/src/main/java/kr/hs/anu/nobet/presentation/screen/login/LoginActivity.kt
@@ -1,5 +1,7 @@
 package kr.hs.anu.nobet.presentation.screen.login
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -7,6 +9,8 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import kr.hs.anu.nobet.R
 import kr.hs.anu.nobet.databinding.ActivityLoginBinding
+import androidx.core.net.toUri
+import kr.hs.anu.nobet.utils.openPage
 
 class LoginActivity : AppCompatActivity() {
 
@@ -26,6 +30,15 @@ class LoginActivity : AppCompatActivity() {
         // logo 누르면 홈으로 이동
         binding.ivTopbarLogo.setOnClickListener {
             finish()
+        }
+
+        binding.btnLogin.setOnClickListener {
+            //TODO 로그인 로직 구현 예정
+        }
+
+        // 온라인 상담 바로가기
+        binding.tvMobile.setOnClickListener {
+            this.openPage()
         }
     }
 }
