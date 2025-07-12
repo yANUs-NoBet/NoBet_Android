6f477c96617976f4391d300876b92cef92fbbbc6
bhindor <bhindor@dgsw.hs.kr>
2025-07-12 13:48:56 +0900

    ⚡️ :: recycler dummy data
    허용 사이트 리사이클러 + 더미 데이터 작업

--- app/src/main/java/kr/hs/anu/nobet/presentation/screen/allow/AllowActivity.kt
+++ app/src/main/java/kr/hs/anu/nobet/presentation/screen/allow/AllowActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import kr.hs.anu.nobet.R
 import kr.hs.anu.nobet.databinding.ActivityAllowBinding
 import kr.hs.anu.nobet.utils.openPage
@@ -14,6 +15,7 @@ class AllowActivity : AppCompatActivity() {
 
     private val viewModel : AllowViewModel by viewModels()
     private lateinit var binding : ActivityAllowBinding
+    private lateinit var allowAdapter: AllowRecyclerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,6 +34,16 @@ class AllowActivity : AppCompatActivity() {
 
         binding.tvMobile.setOnClickListener {
             this.openPage()
+        }
+
+        allowAdapter = AllowRecyclerAdapter()
+        binding.recyclerAllow.apply {
+            layoutManager = LinearLayoutManager(this@AllowActivity)
+            adapter = allowAdapter
+        }
+
+        viewModel.siteList.observe(this) { list ->
+            allowAdapter.submitList(list)
         }
     }
 }
---
+++ app/src/main/java/kr/hs/anu/nobet/presentation/screen/allow/AllowRecyclerAdapter.kt
@@ -0,0 +1,46 @@
+package kr.hs.anu.nobet.presentation.screen.allow
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kr.hs.anu.nobet.databinding.AllowItemBinding
+import kr.hs.anu.nobet.domain.model.AllowSiteData
+
+class AllowRecyclerAdapter : RecyclerView.Adapter<AllowRecyclerAdapter.AllowViewHolder>() {
+
+    private val itemList = mutableListOf<AllowSiteData>()
+
+    fun submitList(newList: List<AllowSiteData>) {
+        itemList.clear()
+        itemList.addAll(newList)
+        notifyDataSetChanged()
+    }
+
+    inner class AllowViewHolder(private val binding: AllowItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: AllowSiteData) {
+            //TODO 데이터 넣어주기
+            binding.tvAllowSite.text = item.siteUrl
+
+            binding.btnDel.setOnClickListener {
+                //TODO 삭제
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AllowViewHolder {
+        val binding = AllowItemBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return AllowViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: AllowViewHolder, position: Int) {
+        holder.bind(itemList[position])
+    }
+
+    override fun getItemCount(): Int = itemList.size
+}
