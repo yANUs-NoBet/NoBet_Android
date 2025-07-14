a5e3d8bfb3a0c0fdfbee2a9731db03dc991ecdc7
bhindor <bhindor@dgsw.hs.kr>
2025-07-14 14:17:28 +0900

    ⚡️ :: block list file
    차단 사이트 목록 파일 생성

---
+++ app/src/main/java/kr/hs/anu/nobet/presentation/screen/block_list/BlockListActivity.kt
@@ -0,0 +1,26 @@
+package kr.hs.anu.nobet.presentation.screen.block_list
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kr.hs.anu.nobet.R
+import kr.hs.anu.nobet.databinding.ActivityBlockListBinding
+
+class BlockListActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityBlockListBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityBlockListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
