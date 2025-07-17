5afb74b976503aba32a3a8d0b866c493a2c0aa01
bhindor <bhindor@dgsw.hs.kr>
2025-07-17 10:23:57 +0900

    ⚡️ :: add report activity
    신고하기 추가

---
+++ app/src/main/java/kr/hs/anu/nobet/presentation/screen/report/ReportActivity.kt
@@ -0,0 +1,26 @@
+package kr.hs.anu.nobet.presentation.screen.report
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kr.hs.anu.nobet.R
+import kr.hs.anu.nobet.databinding.ActivityReportBinding
+
+class ReportActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityReportBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityReportBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
